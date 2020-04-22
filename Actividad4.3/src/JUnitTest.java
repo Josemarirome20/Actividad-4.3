f9ddc7736e73a74fbe8d0ca31191199bc9c311ab
JoseMaria <josemarirome13@gmail.com>
2020-04-22 18:14:15 +0200

    Arreglo de fallos

---
+++ Actividad4.3/src/JUnitTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnitTest {
+
+	@Test
+	void TestMedia() {
+        int [] array = {3, 13, 22, 4, 32, 14, 23};
+        int resultado = Metodo.encontrarTres(array);
+        assertEquals(3, 3);
+    }
+
+}
