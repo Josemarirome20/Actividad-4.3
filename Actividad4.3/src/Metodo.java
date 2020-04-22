dc39463a891fda3d3e5afd32f2977740a3929421
Salvador <salvatapiaparra@gmail.com>
2020-04-22 17:49:00 +0200

    En el main se llama a la clase y en la clase del metodo se incluye el metodo

--- Actividad4.3/src/Metodo.java
+++ Actividad4.3/src/Metodo.java
@@ -11,4 +11,21 @@ public class Metodo {
 		System.out.println("La Media es : " + resultado);
 		return resultado;
 	}
+public static int encontrarTres(String[]Array3) {
+		
+		int contador=0;
+		
+		for(int i=0;i<Array3.length;i++) {
+			if(Array3.equals("3")) {
+				contador++;
+				System.out.println("El número de coincidencias con "+ Array3[1]+" son: "+contador);
+			}else {
+				System.out.println("No hay ninguna coincidencia");
+			}
+			
+		}
+		
+		int resultado=0;
+		return resultado;
+	}
 }
