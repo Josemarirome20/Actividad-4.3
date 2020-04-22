862aceec13ee06c696c9b9bb7dcd50813503d2e7
JoseMaria <josemarirome13@gmail.com>
2020-04-22 19:07:13 +0200

    Añadida la segunda prueba de JUnit ya que a Salvador no le funciona

--- Actividad4.3/src/JUnitTest.java
+++ Actividad4.3/src/JUnitTest.java
@@ -5,10 +5,17 @@ import org.junit.jupiter.api.Test;
 class JUnitTest {
 
 	@Test
-	void TestMedia() {
+	void TestEncontrar() {
         int [] array = {3, 13, 22, 4, 32, 14, 23};
         int resultado = Metodo.encontrarTres(array); 
         assertEquals(3, resultado);
     }
+	
+	  @Test
+	    void TestMedia() {
+	        int [] array = {3, 13, 22, 4, 32, 14, 23};
+	        int resultado = Metodo.CalcularMedia(array);
+	        assertEquals(15, resultado);
+	    }
 
 }
