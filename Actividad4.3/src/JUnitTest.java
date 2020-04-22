0377750d33f7c52b94e158b43a0f1189c77fdfe0
JoseMaria <josemarirome13@gmail.com>
2020-04-22 19:03:14 +0200

    Prueba de commit

--- Actividad4.3/src/JUnitTest.java
+++ Actividad4.3/src/JUnitTest.java
@@ -7,7 +7,7 @@ class JUnitTest {
 	@Test
 	void TestMedia() {
         int [] array = {3, 13, 22, 4, 32, 14, 23};
-        int resultado = Metodo.encontrarTres(array);
+        int resultado = Metodo.encontrarTres(array); 
         assertEquals(3, resultado);
     }
 
